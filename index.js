b810da1dc6f03a7b570b38debda493f73066469f
tanvir005 <tanvir3550gmail.com>
2022-05-08 22:21:32 +0600

    Inventoryitems post api created

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xz1rk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryItemsCollections = client.db('WAREHOUSE-INVENTORY-MANAGEMENT').collection('invenrotyitems');
+
+        app.post('/invenrotyitems', async (req, res) => {
+            const doc = req.body;
+            const result = await serviceCOllection.insertOne(doc);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+
 app.get('/', (req, res) => {
     res.send('server running');
 });
